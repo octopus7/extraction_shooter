688a19fedb3594e7b818d0627bad86b0fd09cb36
octopus7 <octopus7net@gmail.com>
2026-05-29 17:28:02 +0900

    레이저 NS 위치조정

---
+++ TunaSweeper/Source/TunaSweeper/Public/UI/TunaSweeperShopSellPanelWidget.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+#include "TunaSweeperShopSellPanelWidget.generated.h"
+
+class UBorder;
+class UButton;
+class UTextBlock;
+
+UCLASS(BlueprintType, Blueprintable)
+class TUNASWEEPER_API UTunaSweeperShopSellPanelWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "TunaSweeper|Shop")
+	void RefreshSelectedItem();
+
+	UFUNCTION(BlueprintCallable, Category = "TunaSweeper|Shop")
+	bool TrySellSelectedHoveredItem();
+
+protected:
+	virtual TSharedRef<SWidget> RebuildWidget() override;
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	virtual void NativePreConstruct() override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TunaSweeper|Shop", meta = (BindWidgetOptional))
+	TObjectPtr<UBorder> PanelBackground;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TunaSweeper|Shop", meta = (BindWidgetOptional))
+	TObjectPtr<UTextBlock> ItemNameText;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TunaSweeper|Shop", meta = (BindWidgetOptional))
+	TObjectPtr<UTextBlock> SalePriceText;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TunaSweeper|Shop", meta = (BindWidgetOptional))
+	TObjectPtr<UButton> SellButton;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TunaSweeper|Shop", meta = (BindWidgetOptional))
+	TObjectPtr<UTextBlock> SellButtonText;
+
+private:
+	void BuildNativeWidgetTree();
+	void CacheNamedWidgets();
+
+	UFUNCTION()
+	void HandleSellButtonClicked();
+};
