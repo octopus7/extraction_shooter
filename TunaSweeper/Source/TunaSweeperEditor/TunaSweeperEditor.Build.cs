b7c1965875f3f3ee53ba59c6a3bb6919137b965b
octopus7 <octopus7net@gmail.com>
2026-09-15 03:06:04 +0900

    Remove one-shot lava generators after asset validation

--- TunaSweeper/Source/TunaSweeperEditor/TunaSweeperEditor.Build.cs
+++ TunaSweeper/Source/TunaSweeperEditor/TunaSweeperEditor.Build.cs
@@ -7,8 +7,6 @@ public class TunaSweeperEditor : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		// Keep editor tools independent of accidental unity-build includes.
 		bUseUnity = false;
-		PrivateIncludePaths.Add(System.IO.Path.Combine(EngineDirectory, "Plugins/FX/Niagara/Source/Niagara/Internal"));
-		PrivateIncludePaths.Add(System.IO.Path.Combine(EngineDirectory, "Plugins/FX/Niagara/Source/NiagaraShader/Internal"));
 
 		PrivateDependencyModuleNames.AddRange(new string[]
 		{
@@ -34,11 +32,6 @@ public class TunaSweeperEditor : ModuleRules
 			"ToolMenus",
 			"UATHelper",
 			"TunaSweeper",
-			"Niagara",
-			"NiagaraEditor",
-			"NiagaraShader",
-			"MeshDescription",
-			"StaticMeshDescription",
 		});
 	}
 }
